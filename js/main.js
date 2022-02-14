6b54043d19499cd1ff0cd5440f31d0a2332bd82c
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-02-14 21:59:40 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 let elopenmodal = document.getElementById('open');
 let elClosemodal = document.getElementById('close');
+let elbody = document.getElementById('body')
 let elopen = document.getElementById('sidebar');
 
 elopenmodal.addEventListener('click', function () {
   elopen.classList.add('show');
   elopenmodal.classList.add('hide');
+  elbody.classList.add('left');
 });
 
 elClosemodal.addEventListener('click', function () {
   elopenmodal.classList.remove('hide');
   elopen.classList.remove('show');
+  elbody.classList.remove('left');
 });
 
 var typed = new Typed('#element', {
@@ -44,4 +47,23 @@ document.addEventListener("mousemove", function(b){
   }
 });
 
+var topbutton = document.getElementById('up');
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    topbutton.style.opacity = "1";
+    topbutton.style.transition = "all 0.5s linner";
+  } else {
+    topbutton.style.opacity = "0";
+    topbutton.style.transition = "all 0.5s linner";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
